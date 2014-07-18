d6bccfb37459291c4d1b62ce970dd558dfc471c4
ittekikun <ittekikun@hotmail.co.jp>
2014-07-18 21:05:20 +0900

    画像生成コードをUtilityクラスへしたことで簡略化

--- src/main/java/com/ittekikun/plugin/MineTweet/Utility.java
+++ src/main/java/com/ittekikun/plugin/MineTweet/Utility.java
@@ -4,7 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import twitter4j.TwitterException;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -266,5 +270,82 @@ public class Utility
 		}
 
 		log.info("Received "+i+"players: "+message);
+	}
+
+	public static void generationPlayerImage(String playerName,String message) throws TwitterException
+	{
+		BufferedImage base = null;
+		BufferedImage head = null;
+		BufferedImage name = null;
+		BufferedImage mes = null;
+		try
+		{
+			base = new BufferedImage(600, 200, BufferedImage.TYPE_INT_BGR);
+
+			head = ImageIO.read(new URL("https://minotar.net/avatar/" + playerName + "/200.png"));
+
+			name = new BufferedImage(400, 100, BufferedImage.TYPE_INT_BGR);
+
+			mes = new BufferedImage(400, 100, BufferedImage.TYPE_INT_BGR);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		//白く塗りつぶし
+		Graphics2D baseGraphics = base.createGraphics();
+		baseGraphics.setColor(Color.WHITE);
+		baseGraphics.fillRect(0,0,600,200);
+
+		//白く塗りつぶし
+		Graphics2D nameGraphics = name.createGraphics();
+		nameGraphics.setColor(Color.WHITE);
+		nameGraphics.fillRect(0,0,400,100);
+
+		//色々して文字列書き込み
+		nameGraphics.setColor(Color.BLACK);
+		Font nameFont = new Font("Monospaced",Font.PLAIN,50);
+		nameGraphics.setFont(nameFont);
+		nameGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		drawStringCenter(nameGraphics,400,100,playerName);
+
+		//白く塗りつぶし
+		Graphics2D mesGraphics = mes.createGraphics();
+		mesGraphics.setColor(Color.WHITE);
+		mesGraphics.fillRect(0,0,400,100);
+
+		//色々して文字列書き込み
+		mesGraphics.setColor(new Color(0,167,212));
+		Font f = new Font("Monospaced",Font.BOLD,45);
+		mesGraphics.setFont(f);
+		mesGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		drawStringCenter(mesGraphics,400,100,message);//16文字まで
+
+		//ベースに統合
+		baseGraphics.drawImage(head, 0, 0, null);
+		baseGraphics.drawImage(name, 200, 0, null);
+		baseGraphics.drawImage(mes, 200, 100, null);
+
+		//	ファイル保存
+		try
+		{
+			ImageIO.write(base, "png", new File(MineTweet.instance.getDataFolder(), "temp.png"));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void drawStringCenter(Graphics g, int x,int y, String text)
+	{
+		Rectangle size = new Rectangle(x, y);
+		FontMetrics fm = g.getFontMetrics();
+		Rectangle rectText = fm.getStringBounds(text, g).getBounds();
+		int nx = (size.width - rectText.width) / 2;
+		int ny = (size.height - rectText.height) / 2 + fm.getMaxAscent();
+		// Draw text
+		g.drawString(text, nx, ny);
 	}
 }
